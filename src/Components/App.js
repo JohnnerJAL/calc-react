b7a8d7cafcc8c1da615d75bbd80239d8dc5c2755
Johnner Linux <johnner@outlook.com>
2021-01-18 09:50:05 -0500

    Starting project

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Screen from "./Screen";
+import Buttons from "./Buttons";
+import "./Styles/App.css";
+
+function App() {
+  return (
+    <section className="container">
+      <Screen/>
+      <Buttons/>
+    </section>
+  )
+}
+
+export default App;
---
+++ src/Components/Screen.js
@@ -0,0 +1,14 @@
+import { React, Component } from 'react';
+import "./Styles/screen.css";
+
+class Screen extends Component {
+  render() {
+    return (
+      <div className="screen" id="screen">
+        
+      </div>
+    )
+  }
+}
+
+export default Screen;
